acb3c98094a6384ea2fa9809294419e3409a97cd
Mark Florence <mflo999@gmail.com>
2018-03-24 18:29:36 -0400

    Start to add components

---
+++ src/pipes/ellipsize.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+/**
+ * Truncates a long string after N characters, then appends an ellipsis.
+ * By default, N is 100.
+ *
+ * <a [href]="{{'very_long_URL'}}">
+ *   {{'very_long_URL' | libEllipsize:64 | libBreakable}}
+ * </a>
+ *
+ */
+
+@Pipe({ name: 'libEllipsize' })
+
+export class EllipsizePipe implements PipeTransform {
+
+  transform(s: string,
+            max = 100,
+            dflt = ''): string {
+    if (s == null)
+      return dflt;
+    else return (s.length > max)? `${s.substring(0, max)}\u2026` : s;
+  }
+
+}
