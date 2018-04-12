2709f0b3e5a948344a08723da4e760be6d02ef3b
Mark Florence <mflo999@gmail.com>
2018-04-12 16:11:58 -0400

    Prepare to publish

--- lib/directives/rxf-submit.d.ts
+++ lib/directives/rxf-submit.d.ts
@@ -1,6 +1,6 @@
 import { FormGroup } from '@angular/forms';
 /**
- * A directive to handle form submit via calueChanges
+ * A directive to handle form submit via valueChanges
  */
 export declare class RxfSubmitDirective {
     libRxfSubmit: FormGroup;
