5279ecec210327a8bccfa13ffb25c2c8d885afde
Mark Florence <mflo999@gmail.com>
2019-02-28 15:23:43 -0500

    One more try

--- src/decorators/debounce.ts
+++ src/decorators/debounce.ts
@@ -4,25 +4,14 @@
  * @see https://stackoverflow.com/questions/44634992
  */
 
-export function Debounce(wait = 0,
-                         immediate = false): MethodDecorator {
+export function Debounce(delay = 0): MethodDecorator {
   return function (target: any,
                    propertyKey: string,
                    descriptor: PropertyDescriptor) {
     const original = descriptor.value;
-    descriptor.value = function () {
-      let timeout = null;
-      const context = this;
-      const args = arguments;
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(function () {
-        timeout = null;
-        if (!immediate)
-          original.apply(context, args);
-      }, wait);
-      if (callNow)
-        original.apply(context, args);
+    descriptor.value = function (...args) {
+      clearTimeout(this.__timeout);
+      this.__timeout = setTimeout(() => original.apply(this, args), delay);
     };
     return descriptor;
   };
