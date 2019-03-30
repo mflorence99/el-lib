8d58b08bd04c7e0331a7a7dcfc7b408c460a6064
Mark Florence <mflo999@gmail.com>
2019-03-30 08:05:28 -0400

    Simplify publish

--- publish.sh
+++ publish.sh
@@ -2,20 +2,20 @@ set -e
 npm run build
 npm version patch
 npm publish
-sleep 15s
-pushd ../el-3270
-rm -rf node_modules/ellib
-npm install
-popd
-pushd ../el-aws
-rm -rf node_modules/ellib
-npm install
-popd
-pushd ../el-term
-rm -rf node_modules/ellib
-npm install
-popd
-pushd ../el-file
-rm -rf node_modules/ellib
-npm install
-popd
+# sleep 15s
+# pushd ../el-3270
+# rm -rf node_modules/ellib
+# npm install
+# popd
+# pushd ../el-aws
+# rm -rf node_modules/ellib
+# npm install
+# popd
+# pushd ../el-term
+# rm -rf node_modules/ellib
+# npm install
+# popd
+# pushd ../el-file
+# rm -rf node_modules/ellib
+# npm install
+# popd
